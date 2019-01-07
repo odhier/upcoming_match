58ed7318305b872aacfc66a8262387e42d86ee48
odhier <odhier1@gmail.com>
2019-01-07 19:28:58 +0700

    First init

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php 
+/**
+* Plugin Name: Upcoming Match
+* Plugin URI: https://agenkaisar.online/
+* Description: Free Upcoming Match Plugin
+* Version: 1.0
+* Author: Odhier
+* Author URI: https://agenkaisar.online/
+*/
+
+/*
+* Creating a function to create our CPT
+*/
