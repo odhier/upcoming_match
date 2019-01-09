fc3d2d6725dd13579c794e25aea234c677d5c950
odhier <odhier1@gmail.com>
2019-01-09 16:23:44 +0700

    Admin Select Match

--- menu.php
+++ menu.php
@@ -10,11 +10,16 @@
         <td>
             <!-- <input type="color" id="id_match" name="id_match" class="id_match" value="<?php echo esc_attr( get_option('id_match') ); ?>"> -->
            <select name="_id_match" id="_id_match">
-               <option value="123">Liverpool vs Mancity</option>
-               <option value="123">Liverpool vs Mancity</option>
-               <option value="123">Liverpool vs Mancity</option>
-               <option value="123">Liverpool vs Mancity</option>
-           </select>
+           <?php 
+           global $wpdb;
+           $matches = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}upmatches WHERE status='not_started'", ARRAY_A ); 
+           foreach($matches as $match){
+               $date = date('d/m/Y H:i:s',strtotime($match['match_datetime'] ." +7 hours")); 
+            ?>
+               <option <?php echo (esc_attr( get_option('_id_match')) == $match['match_id'])?'selected':'';?> value="<?php echo $match['match_id'];?>"><?php echo "<b>".$match['teamh']."</b> VS <b>".$match['teama']."</b> (".$date.")";?></option> 
+            <?php } ?>
+            
+               </select>
         </tr>
     </table>
     
